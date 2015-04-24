d8158b0289aa30111b2dbbde0de24ff6635df9e2
Ryan Yurkanin <me@ryanyurkan.in>
2015-04-24 12:02:28 -0400

    pls

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express      	   = require('express'),
     http    	 	   = require('http'),
     app     	 	   = express(),
     server  	 	   = http.createServer(app),
-    port    	 	   = 8080,
+    port    	 	   = 80,
     game_sockets 	   = {},
     controller_sockets = {};
 
